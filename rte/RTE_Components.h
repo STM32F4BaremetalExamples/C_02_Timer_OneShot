431c6e70381ffc525db3f92fd7952d614e982de4
Manuel Limones <limonesu.me@gmail.com>
2014-12-13 02:13:07 -0600

    Rename proyect files to match git repository

--- rte/RTE_Components.h
+++ rte/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'C_02_Timer_STM32F4' 
+ * Project: 'C_02_Timer_OneShot' 
  * Target:  'Target 1' 
  */
 
